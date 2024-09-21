1ff7cb292d448e3521875543e1311e094838b8c0
Sydnes <136016959+Sydnes2003@users.noreply.github.com>
2024-09-22 01:55:28 +0300

    add Header, add Navbar, minor style changes

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,20 +1,26 @@
 import { routeConfig } from "@/shared/config/routeConfig";
 import { Route, Routes } from "react-router-dom";
-import { Suspense } from "react";
+import { FC, Suspense } from "react";
 
-const AppRouter = () => {
+interface AppRouterProps {
+    className?: string;
+}
+
+const AppRouter: FC<AppRouterProps> = ({className = ""}) => {
     return (
         // todo: Fallback
         <Suspense fallback={null}>
-            <Routes>
-                {Object.values(routeConfig).map((route) => (
-                    <Route
-                        key={route.path}
-                        path={route.path}
-                        element={route.element}
-                    />
-                ))}
-            </Routes>
+            <div className={className}>
+                <Routes>
+                    {Object.values(routeConfig).map((route) => (
+                        <Route
+                            key={route.path}
+                            path={route.path}
+                            element={route.element}
+                        />
+                    ))}
+                </Routes>
+            </div>
         </Suspense>
     );
 };
